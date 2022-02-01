9bc2c24b89b9a7ad7fb420369a8427d864090a21
marques173 <juliocesar173@hotmail.com>
2022-02-01 02:42:29 -0300

    java

---
+++ script.js
@@ -0,0 +1,31 @@
+//alert`Olá, MUndo!!`
+//alert`Estou aprendendo a programar em JAVASCRITP`
+//window.prompt`Olá qual é seu nome ?`
+
+
+//var nome = window.prompt`qual é seu nome`
+//window.alert(`prazer em ti conhecer , ` + nome + '!')
+//window.confirm`Esta gostando do programa`
+//var n1 = Number.parseFloat(window.prompt(`Digite um Numero`))
+//var n2 = Number.parseFloat(window.prompt(`Digite outro Numero`))
+//var res = n1 + n2
+//window.alert(`a soma entre os valores são ` + res)
+
+//var n1 = Number(window.prompt(`digite `))
+//var n2 = Number(window.prompt(`outro numero`))
+//var res = n1 + n2
+//window.alert(`a soma entre ${n1} e ${n2} é igual a ${res} `)
+
+//window.alert(`a soma dos valores é ` + res)
+
+var nome = window.prompt('Qual é seu nome?')
+document.writeln(` Olá, muito prazer ${nome} Seu nome tem ${nome.length} <br>letras`)
+document.write(`Seu nome em maiusculo é ${nome.toUpperCase()} `)
+document.write(`Seu nome em minusculo é ${nome.toLowerCase()}`)
+
+
+
+
+
+
+
